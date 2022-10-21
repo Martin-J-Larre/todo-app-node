1d20842990a38bf15d809607aadb871d7f0e3b5d
Martin-J-Larre <martinjlarre@gmail.com>
2022-10-21 16:18:17 -0400

    #2 router started

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("working");
+const express = require('express');
+app = express(); 
+
+// imports
+const router = require('./routes/todoListRouter');
+
+const PORT= 5000;
+
+//middlewares
+app.use(express.json());
+
+//routes
+app.use('/api', router);
+
+
+
+app.listen(PORT, () =>{
+    console.log(`Server is listening on port http://localhost:${PORT}`);
+});
