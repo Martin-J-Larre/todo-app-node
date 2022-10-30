bbe4869eb0dac19183035ad59907c31cc7737bc1
Martin-J-Larre <martinjlarre@gmail.com>
2022-10-30 01:56:47 -0400

    #7 controller getOneTodo finished

--- controllers/todoListController.js
+++ controllers/todoListController.js
@@ -1,19 +1,29 @@
 const Todo = require('../models/todoListModel');
 
 const getTodos = async (req, res)=>{
-    
+    try {
+        const todos = await Todo.find({})
+        res.status(200).json({todos})
+    } catch (err) {
+        res.status(500).json({message: err});
+    }
 
 }
-const createTodo = async (req, res)=>{
+const createTodo = async (req, res) =>{
     try {
         const todo = await Todo.create(req.body);
         res.status(201).json({todo});
     } catch (err) {
-        console.log(err);
+        res.status(500).json({message: err});
     }
 }
-const getOneTodo = (req, res)=>{
-    res.send('getone');
+const getOneTodo = async (req, res) =>{
+    try {
+        const todoId = await Todo.findById(req.params.id);
+        res.status(200).json({todoId})
+    } catch (err) {
+        res.status(500).json({message: err});
+    }
 }
 const updateTodo = (req, res)=>{
     res.send('update one');
--- models/todoListModel.js
+++ models/todoListModel.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
 const TodoListSchema = new mongoose.Schema({
-	title: String,
-	completed: Boolean,					
+	title: {
+		type:String,
+		required: [true, 'You should enter some todo'],
+		trim: true,
+	},
+	completed: {
+		type: Boolean,
+		default: false,
+	},					
   });
 
 module.exports = mongoose.model('Todo', TodoListSchema);	
